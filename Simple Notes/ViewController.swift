c655001b63624bad270e8372bfc8e8b55b56f7b6
kvillnv <kvillnv@gmail.com>
2017-04-13 02:03:23 -0400

    CoreData NSManaged classes created / Group folders / First views Classes are similar to Android version, but there is no data collection, because it seems the data collection is the data base - And it seems Authentication has to have a relationship with notes, so I put One to Many and maybe we will not need the "passwordProtected" boolean

---
+++ Simple Notes/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Simple Notes
+//
+//  Created by David Winnicki on 2017-04-12.
+//  Copyright © 2017 Winnicki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var number: Int!
+    var name: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var _: String!
+        
+        var _: Int!
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func testFunc() -> Void {
+        // Empty func
+    }
+
+}
+
