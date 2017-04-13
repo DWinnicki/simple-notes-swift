6dbab16088b2c02847a6771946d6f6e0ea5252cb
kvillnv <kvillnv@gmail.com>
2017-04-13 01:44:58 -0400

    CoreData NSManaged classes created / Group folders / First views Classes are similar to Android version, but there is no data collection, because it seems the data collection is the data base - And it seems Authentication has to have a relationship with notes, so I put One to Many and maybe we will not need the "passwordProtected" boolean

---
+++ Note+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Note+CoreDataClass.swift
+//  Simple Notes
+//
+//  Created by Karl J. Villeneuve on 2017-04-13.
+//  Copyright © 2017 Winnicki. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+@objc(Note)
+public class Note: NSManagedObject {
+
+}
